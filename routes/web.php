c03f96e390796b7b78ca621e9f709506379b7b64
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-04-11 14:57:50 +0500

    Create route and alll for [blade}'s written route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
+    Route::get('/login',[SiteController::class, 'login'])->name('login');
+    Route::get('/registar',[SiteController::class, 'registar'])->name('registar');
+    Route::get('/task-create',[SiteController::class, 'task-create'])->name('task-create');
+    Route::get('/task-edit',[SiteController::class, 'task-edit'])->name('task-edit');
+    Route::get('/task',[SiteController::class, 'task'])->name('task');
+    
+    
+    
+    
+    
+
 });
+
+
+
+
+
+
+
+
